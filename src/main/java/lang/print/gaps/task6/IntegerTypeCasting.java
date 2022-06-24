345d1c50c1101440308e1d119a8ade6f8feb0194
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-06-24 13:35:19 +0300

    Update IntegerTypeCasting.java

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -4,6 +4,7 @@ public class IntegerTypeCasting {
     public static void main(String[] args) {
         long first = 1000L;
         int second = (int)first;
-        System.out.println(first + "\n" + second);       
+        System.out.println(first);  
+        System.out.println(second);
     }
 }
